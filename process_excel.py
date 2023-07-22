cf73f7ba0b7d810253a004902eec80097ea89f2c
xexenjoyer <108595741+xexenjoyer@users.noreply.github.com>
2023-07-22 14:44:10 +0300

    Update process_excel.py

--- process_excel.py
+++ process_excel.py
@@ -1,12 +1,15 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import json
+import webbrowser
+import os
 
 def get_available_columns(file_path):
     df = pd.read_excel(file_path)
     columns = ', '.join(df.columns)
     return columns
 
-def process_excel(file_path, selected_columns, merged_columns):
+def process_excel(file_path, selected_columns, merged_columns,restrictions):
     # Чтение данных из Excel-файла
     df = pd.read_excel(file_path)
 
@@ -23,10 +26,30 @@ def process_excel(file_path, selected_columns, merged_columns):
         if not merged_columns[i]:
             merged_columns.pop(i)
 
+    #Фильтры
+    restrictions = restrictions.split(',')
+    for i in range(len(restrictions)-1,-1,-1):
+        restrictions[i]=restrictions[i].strip().split("=")
+        if not restrictions[i]:
+            restrictions.pop(i)
+
     not_merged_columns = list(set(selected_columns) - set(merged_columns))
 
     # Фильтрация данных по выбранным столбцам
     df = df[selected_columns]
+
+
+
+    # Фильтрация данных по содержанию строк
+    if len(restrictions[0][0])!=0:
+        for i in range(len(restrictions)):
+            col = restrictions[i][0]
+            df[col] = df[col].astype(str)
+            df = df[df[col] == restrictions[i][1]]
+
+
+
+
 
     # Преобразование каждой строки в JSON-объект
     converted_rows = [False for i in range(len(df.index))]
@@ -38,7 +61,9 @@ def process_excel(file_path, selected_columns, merged_columns):
                 if not converted_rows[j] and df.loc[i][not_merged_columns].equals(df.loc[j][not_merged_columns]):
                     converted_rows[j] = True
                     for col in merged_columns:
+                        flag = 0
                         col_i = df.loc[i][col]
+
                         col_j = df.loc[j][col]
                         if type(col_i) is not list and col_i != col_j:
                             col_i = [col_i, col_j]
@@ -48,5 +73,13 @@ def process_excel(file_path, selected_columns, merged_columns):
             json_objects.append(df.loc[i].to_dict())
 
     # Запись JSON-объектов в текстовый файл
-    with open('output.json', 'w') as file:
-        json.dump(json_objects, file)
+
+    with open('output/output.json', 'w') as file:
+        # исправить работу с кириллицей
+        json.dump(json_objects, file,ensure_ascii=False)
+
+        #df = pd.read_json(r'output.json', encoding='cp1251')
+
+        # открыть json'чик
+        pythonfile = os.path.abspath('output/output.json')
+        webbrowser.open(pythonfile)
